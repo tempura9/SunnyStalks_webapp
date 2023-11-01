1b642c5cd62a53dc11afc2a51f968a5fdcb5dd7a
lmfaoxd <waynessantos@gmail.com>
2023-11-02 02:35:40 +0800

    front end

--- webapp/tracker/urls.py
+++ webapp/tracker/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("grocer/add",views.addGrocer),
+    path("edit/<int:id>",views.editGrocer),
+    path("grocer/<int:id>",views.grocerDetails),
+    path("purchase/add",views.groceryPurchase),
+    path("compare",views.compareGrocers),
 ]
--- webapp/tracker/views.py
+++ webapp/tracker/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "tracker/index.html", {})
+def addGrocer(request):
+    pass
+def editGrocer(request):
+    pass
+def grocerDetails(request):
+    pass
+def groceryPurchase(request):
+    pass
+def compareGrocers(request):
+    pass
